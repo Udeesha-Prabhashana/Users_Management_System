c0c1963f506903e43fd88d2d1b595f0b524406c8
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-09-06 23:22:35 +0530

    add user update method in backend

--- userhandlesystem-backend/userhandlesystem-backend/src/main/java/com/udeesha/userhandlesystembackend/controller/UserController.java
+++ userhandlesystem-backend/userhandlesystem-backend/src/main/java/com/udeesha/userhandlesystembackend/controller/UserController.java
@@ -31,4 +31,16 @@ public class UserController {
                 .orElseThrow(()->new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepositary.findById(id)
+                .map(user ->{
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepositary.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+
+    }
+
 }
