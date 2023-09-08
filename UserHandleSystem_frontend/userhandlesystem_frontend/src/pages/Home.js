c4c6f99d4e406dc352a3d9922b64ee2ecbbd4a66
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-09-08 22:46:48 +0530

    complete the delete function

--- UserHandleSystem_frontend/userhandlesystem_frontend/src/pages/Home.js
+++ UserHandleSystem_frontend/userhandlesystem_frontend/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
 
   const [Users, setUsers] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {     //when Home page is load , then will open inside of the useEffect hook
     loadUsers();
@@ -15,6 +16,12 @@ export default function Home() {
     setUsers(result.data)
     console.log(result.data);
   }
+
+  const HandleDelete = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${ id }`)
+    loadUsers()
+  }
+
   return (
     <div className="container">
       <div className="py-4">
@@ -40,7 +47,7 @@ export default function Home() {
                 <td>
                   <button className="btn btn-primary mx-2">View </button>
                   <Link className="btn btn-outline-primary mx-2" to={`/editUser/${User.id}`}> Edit</Link>
-                  <button className="btn btn-danger mx-2"> Delete </button>
+                  <button className="btn btn-danger mx-2" onClick={()=> HandleDelete(User.id)}> Delete </button>
                 </td>
               </tr>
             ))}
